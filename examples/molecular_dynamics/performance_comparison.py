9a0fd40b8df56b28e096f607178d07ab5453d00d
Aditya Bhosale <adityapb1546@gmail.com>
2020-06-06 00:33:53 +0530

    Add print to debug

--- examples/molecular_dynamics/performance_comparison.py
+++ examples/molecular_dynamics/performance_comparison.py
@@ -1,14 +1,12 @@
 import numpy as np
 import time
-from md_nnps import MDSolver
+import md_simple, md_nnps
 
 from compyle.config import get_config
 
 
-def solve(n, backend, tf=0.5, dt=0.02, use_count_sort=False):
-    solver = MDSolver(n, backend=backend.replace("_omp", ""),
-                      use_count_sort=use_count_sort)
-
+def solve(n, backend, solver_algo, tf=0.5, dt=0.02, use_count_sort=False):
+    solver = solver_algo(n, backend=backend.replace("_omp", ""))
     start = time.time()
     solver.solve(tf, dt)
     end = time.time()
@@ -17,16 +15,17 @@ def solve(n, backend, tf=0.5, dt=0.02, use_count_sort=False):
     return end - start
 
 
-def compare(backends, n_list, niter=3):
+def compare(backends, n_list, solver_algo, niter=3):
     t_list = {b: [] for b in backends}
     speedups = {b: [] for b in backends}
     for n in n_list:
+        print("Running for N = %i" % n)
         for backend in backends:
             if "omp" in backend:
                 get_config().use_openmp = True
             t = 1e9
             for it in range(niter):
-                t = min(t, solve(n, backend))
+                t = min(t, solve(n, backend, solver_algo))
             t_list[backend].append(t)
             if "omp" in backend:
                 get_config().use_openmp = False
@@ -39,6 +38,33 @@ def compare(backends, n_list, niter=3):
         speedups = None
 
     return speedups, t_list
+
+
+def compare_implementations(backend, n_list, niter=3):
+    import matplotlib.pyplot as plt
+    sp, nnps_tlist = compare([backend], n_list,
+                             md_nnps.MDSolver, niter=niter)
+    sp, simple_tlist = compare([backend], n_list,
+                               md_simple.MDSolver, niter=niter)
+
+    speedup = [simple_tlist[backend][i] / nnps_tlist[backend][i]
+            for i in range(len(n_list))]
+
+    plt.loglog(n_list, nnps_tlist[backend], 'x-', label="Linear")
+    plt.loglog(n_list, simple_tlist[backend], 'x-', label="Simple")
+
+    plt.xlabel("Number of particles")
+    plt.ylabel("Time (secs)")
+    plt.legend()
+    plt.savefig("time_comp_impl.png", dpi=300)
+
+    plt.clf()
+
+    plt.loglog(n_list, speedup, 'x-')
+
+    plt.xlabel("Number of particles")
+    plt.ylabel("Speedup")
+    plt.savefig("speedup_comp_impl.png", dpi=300)
 
 
 def plot(n_list, speedups, t_list):
@@ -70,7 +96,15 @@ def plot(n_list, speedups, t_list):
 
 
 if __name__ == "__main__":
-    backends = ["cython", "cython_omp"]
-    n_list = [10000, 40000, 160000, 640000, 2560000, 10240000, 40960000]
-    speedups, t_list = compare(backends, n_list)
-    plot(n_list, speedups, t_list)
+    import sys
+    if len(sys.argv) < 2:
+        backends = ["opencl"]
+        n_list = [10000, 40000, 160000, 640000, 2560000, 10240000, 40960000]
+        print("Running for", n_list)
+        speedups, t_list = compare(backends, n_list, md_nnps.MDSolver)
+        plot(n_list, speedups, t_list)
+    else:
+        backend = "opencl"
+        n_list = [500, 2000, 8000, 32000, 128000, 512000]
+        print("Running for", n_list)
+        compare_implementations(backend, n_list)
