3a0ea574bc80505b155883bf6f0fb420a6968482
Aditya Bhosale <adityapb1546@gmail.com>
2020-06-07 04:55:03 +0530

    Fix openmp scans and update MD performance script

--- examples/molecular_dynamics/performance_comparison.py
+++ examples/molecular_dynamics/performance_comparison.py
@@ -1,14 +1,13 @@
 import numpy as np
 import time
-from md_nnps import MDSolver
+import md_simple
+import md_nnps
 
 from compyle.config import get_config
 
 
-def solve(n, backend, tf=0.5, dt=0.02, use_count_sort=False):
-    solver = MDSolver(n, backend=backend.replace("_omp", ""),
-                      use_count_sort=use_count_sort)
-
+def solve(n, backend, solver_algo, tf=0.5, dt=0.02, use_count_sort=False):
+    solver = solver_algo(n, backend=backend.replace("_omp", ""))
     start = time.time()
     solver.solve(tf, dt)
     end = time.time()
@@ -17,42 +16,79 @@ def solve(n, backend, tf=0.5, dt=0.02, use_count_sort=False):
     return end - start
 
 
-def compare(backends, n_list, niter=3):
+def compare(backends, n_list, solver_algo, niter=3):
     t_list = {b: [] for b in backends}
     speedups = {b: [] for b in backends}
     for n in n_list:
+        print("Running for N = %i" % n)
         for backend in backends:
             if "omp" in backend:
                 get_config().use_openmp = True
             t = 1e9
             for it in range(niter):
-                t = min(t, solve(n, backend))
+                t = min(t, solve(n, backend, solver_algo))
             t_list[backend].append(t)
             if "omp" in backend:
                 get_config().use_openmp = False
 
-    for backend in backends:
-        for i, n in enumerate(n_list):
-            speedups[backend].append(t_list["cython"][i] / t_list[backend][i])
+    if 'cython' in backends:
+        for backend in backends:
+            for i, n in enumerate(n_list):
+                speedups[backend].append(
+                    t_list["cython"][i] / t_list[backend][i])
+    else:
+        speedups = None
 
     return speedups, t_list
 
 
-def plot(n_list, speedups, t_list):
+def compare_implementations(backend, n_list, niter=3):
+    import matplotlib.pyplot as plt
+    sp, nnps_tlist = compare([backend], n_list,
+                             md_nnps.MDSolver, niter=niter)
+    sp, simple_tlist = compare([backend], n_list,
+                               md_simple.MDSolver, niter=niter)
+
+    speedup = [simple_tlist[backend][i] / nnps_tlist[backend][i]
+               for i in range(len(n_list))]
+
+    plt.loglog(n_list, nnps_tlist[backend], 'x-', label="Linear")
+    plt.loglog(n_list, simple_tlist[backend], 'x-', label="Simple")
+
+    plt.xlabel("Number of particles")
+    plt.ylabel("Time (secs)")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("time_comp_impl.png", dpi=300)
+
+    plt.clf()
+
+    plt.loglog(n_list, speedup, 'x-')
+
+    plt.xlabel("Number of particles")
+    plt.ylabel("Speedup")
+    plt.grid(True)
+    plt.savefig("speedup_comp_impl.png", dpi=300)
+
+
+def plot(n_list, speedups, t_list, label):
     backend_label_map = {'cython': 'Cython', 'cython_omp': 'OpenMP',
                          'opencl': 'OpenCL', 'cuda': 'CUDA'}
     import matplotlib.pyplot as plt
     plt.figure()
 
-    for backend, arr in speedups.items():
-        if backend == "cython":
-            continue
-        plt.semilogx(n_list, arr, 'x-', label=backend_label_map[backend])
+    if speedups:
+        for backend, arr in speedups.items():
+            if backend == "cython":
+                continue
+            plt.semilogx(n_list, arr, 'x-', label=backend_label_map[backend])
 
-    plt.xlabel("Number of particles")
-    plt.ylabel("Speedup")
-    plt.legend()
-    plt.savefig("speedup_%s.png" % "_".join(speedups.keys()), dpi=300)
+        plt.xlabel("Number of particles")
+        plt.ylabel("Speedup")
+        plt.legend()
+        plt.grid(True)
+        plt.savefig("%s_speedup_%s.png" %
+                    (label, "_".join(speedups.keys())), dpi=300)
 
     plt.clf()
 
@@ -62,11 +98,42 @@ def plot(n_list, speedups, t_list):
     plt.xlabel("Number of particles")
     plt.ylabel("Time (secs)")
     plt.legend()
-    plt.savefig("time_%s.png" % "_".join(speedups.keys()), dpi=300)
+    plt.grid(True)
+    plt.savefig("%s_time_%s.png" % (label, "_".join(t_list.keys())), dpi=300)
 
 
 if __name__ == "__main__":
-    backends = ["cython", "opencl", "cuda"]
-    n_list = [10000, 40000, 160000, 640000, 2560000, 10240000]
-    speedups, t_list = compare(backends, n_list)
-    plot(n_list, speedups, t_list)
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument(
+        '-c', '--comparison', action='store', dest='comp', default='gpu_comp',
+        choices=['gpu_comp', 'omp_comp', 'comp_algo'],
+        help='Choose the comparison.'
+    )
+    p.add_argument(
+        '--nnps', action='store', dest='nnps', default='linear',
+        choices=['linear', 'simple'],
+        help='Choose algorithm.'
+    )
+
+    o = p.parse_args()
+
+    solver_algo = md_nnps.MDSolver if o.nnps == 'linear' else md_simple.MDSolver
+    n_list = [10000 * (4 ** i) for i in range(6)] if o.nnps == 'linear' else \
+        [500 * (4 ** i) for i in range(5)]
+
+    if o.comp == "gpu_comp":
+        backends = ["opencl", "cuda", "cython"]
+        print("Running for", n_list)
+        speedups, t_list = compare(backends, n_list, solver_algo)
+        plot(n_list, speedups, t_list, o.nnps)
+    elif o.comp == "omp_comp":
+        backends = ["cython_omp", "cython"]
+        print("Running for", n_list)
+        speedups, t_list = compare(backends, n_list, solver_algo)
+        plot(n_list, speedups, t_list, o.nnps)
+    elif o.comp == "comp_algo":
+        backend = "cython"
+        n_list = [500, 1000, 2000, 4000, 8000, 16000, 32000]
+        print("Running for", n_list)
+        compare_implementations(backend, n_list)
